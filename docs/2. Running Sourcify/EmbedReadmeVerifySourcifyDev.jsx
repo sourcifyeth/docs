4d7fee52ccd3af8dfe1af8f4a6e5592534402a5b
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2025-08-05 17:49:31 +0200

    Remove Running Sourcify UI docs, add running verify.sourcify.dev

--- docs/2. Running Sourcify/EmbedReadmeVerifySourcifyDev.jsx
+++ docs/2. Running Sourcify/EmbedReadmeVerifySourcifyDev.jsx
@@ -1,12 +1,12 @@
 import EmbedReadme from "../EmbedReadme";
 import React from "react";
-const repo = "sourcifyeth/ui";
-const branch = "master";
+const repo = "sourcifyeth/verify.sourcify.dev";
+const branch = "main";
 const readmePath = "/README.md";
 const rawBaseUrl = "https://raw.githubusercontent.com";
 const pageBaseUrl = "https://github.com";
 
-export default function EmbedReadmeUI() {
+export default function EmbedReadmeVerifySourcifyDev() {
   return (
     <EmbedReadme
       repo={repo}
